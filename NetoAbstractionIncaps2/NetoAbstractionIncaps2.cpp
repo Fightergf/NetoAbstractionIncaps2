98b817f4d923098c9013e899df175964dcddbaa5
DenisMir <pete4kob12@mail.ru>
2022-08-29 22:26:05 +0300

    work5

--- NetoAbstractionIncaps2/NetoAbstractionIncaps2.cpp
+++ NetoAbstractionIncaps2/NetoAbstractionIncaps2.cpp
@@ -29,50 +29,52 @@ public:
     {
         return Country + ", " + Street + ", " + std::to_string(home) + ", " + std::to_string(room);
     }  //Сборка строки
-    void sortAdress(Address *arrADR, int countadr)
-    {
-        std::string* sM = new std::string[countadr];
-
-        for (int i = 0; i < countadr; i++)
-        {
-            sM[i] = arrADR[i].get_output_address();
-        }
-
-        int res = 0;
-        int listLength = countadr;
-
-        while (listLength--) //Cортировка по алфавиту и сравнение строк
-        {
-            bool swapped = false;
-            for (int i = 0; i < listLength; i++)
-            {
-                res = sM[i].compare(0, 2, sM[i + 1]); //Сравнение строк
-                if (res == 1)
-                {
-                    std::swap(sM[i], sM[i + 1]);
-                    swapped = true;
-                }
-            }
-            if (swapped == false)
-                break;
-        }
-        delete[] arrADR;
-        writeFile(sM, countadr);
-    }
-    void writeFile(std::string *sM, int countadr) //Запись в файл
-    {
-        std::ofstream filew("out.txt");
-
-        filew << countadr << "\n";
-
-        for (int i = 0; i < countadr; i++) 
-        {
-            filew << sM[i] + "\n";
-        }
-        delete[] sM;  //Очистка массива строк
-        std::cout << "Файл записан успешно, проверьте!!!";
-    }
 };
+
+void writeFile(std::string* sM, int countadr) //Запись в файл
+{
+    std::ofstream filew("out.txt");
+
+    filew << countadr << "\n";
+
+    for (int i = 0; i < countadr; i++)
+    {
+        filew << sM[i] + "\n";
+    }
+    delete[] sM;  //Очистка массива строк
+    std::cout << "Файл записан успешно, проверьте!!!";
+}
+
+void sortAdress(Address* arrADR, int countadr)
+{
+    std::string* sM = new std::string[countadr];
+
+    for (int i = 0; i < countadr; i++)
+    {
+        sM[i] = arrADR[i].get_output_address();
+    }
+
+    int res = 0;
+    int listLength = countadr;
+
+    while (listLength--) //Cортировка по алфавиту и сравнение строк
+    {
+        bool swapped = false;
+        for (int i = 0; i < listLength; i++)
+        {
+            res = sM[i].compare(0, 2, sM[i + 1]); //Сравнение строк
+            if (res == 1)
+            {
+                std::swap(sM[i], sM[i + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+    delete[] arrADR;
+    writeFile(sM, countadr);
+}
 
 void out_file()
 {
@@ -90,7 +92,7 @@ void out_file()
         std::cout << "Файл успешно открыт!!!" << std::endl;
         filer >> countAddress;
         int iter = 0;
-        std::cout << countAddress << "Адрессов в наличии!\n";
+        std::cout << countAddress << " - Адрессов в наличии!\n";
 
         Address* arrADR = new Address[countAddress]; //din memory struct
 
@@ -103,7 +105,7 @@ void out_file()
             arrADR[iter] = Address(Country, Street, home, room);
             iter++;
         }
-        ADR.sortAdress(arrADR, countAddress);
+        sortAdress(arrADR, countAddress);
     }
     else
     {
@@ -111,7 +113,6 @@ void out_file()
     }
     filer.close();
 }
-
 
 int main()
 {
